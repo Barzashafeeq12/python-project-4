00a35916c7555455b878c08fc17bf1a765319dee
Barzashafeeq12 <159743547+Barzashafeeq12@users.noreply.github.com>
2025-04-29 00:31:13 +0500

    Add files via upload

---
+++ 01_basics/01_double_it.py
@@ -0,0 +1,8 @@
+curr_value = int(input("Enter a number: "))
+
+# Double the number until it reaches 100 or greater
+while curr_value < 100:
+    # Double the value
+    curr_value = curr_value * 2
+    # Print the new value
+    print(curr_value, end=' ')
---
+++ 01_basics/04_random_numbers.py
@@ -0,0 +1,6 @@
+import random
+
+# Generate and print 10 random numbers
+for i in range(10):
+    value = random.randint(1, 100)
+    print(value, end=' ')
